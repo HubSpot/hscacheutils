132d89486f4363aaaddde194c9599b1de0aa1967
Tim Finley <tfinley@hubspot.com>
2013-03-04 09:23:31 -0500

    setup.py tweaks.

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(name='hscacheutils',
   version='0.1.1',
   description="Some of Hubspot's python cache utils, namely generational caching",
+  long_description=open('README.md').read(),
   author='HubSpot Developer',
   author_email='devteam+hscacheutils@hubspot.com',
   url='http://dev.hubspot.com/',
   license='MIT',
-  packages=['hscacheutils'],
+  packages=find_packages(),
   install_requires=[
     "python-memcached==1.47",
     "Django==1.3",
